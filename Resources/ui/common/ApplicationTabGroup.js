17724b3f3296db7c7c155a541d9bd49af9c7542e
Ignasi Artigas <ignasi.artigas@gmail.com>
2012-11-14 23:10:30 +0100

    Resource images updated

--- Resources/ui/common/ApplicationTabGroup.js
+++ Resources/ui/common/ApplicationTabGroup.js
@@ -6,11 +6,15 @@ function ApplicationTabGroup(ReportWindow, SearchWindow,AboutWindow) {
 	var win1 = new ReportWindow(L('home'));
 	win1.containingTab = Ti.UI.createTab({
 		title: L('home'),
-		icon: '/images/KS_nav_ui.png',
+		
 		window: win1
 	});
-	self.addTab(win1.containingTab);
+	if (Titanium.Platform.name == 'iPhone OS'){
+		win1.containingTab.icon= '/images/KS_nav_ui.png';
+	}
 	
+	self.addTab(win1.containingTab);
+	/*
 	//tab2 : cerca de dades
 	var win2 = new SearchWindow(L('search'));
 	win2.containingTab = Ti.UI.createTab({
@@ -19,7 +23,7 @@ function ApplicationTabGroup(ReportWindow, SearchWindow,AboutWindow) {
 		window: win2
 	});
 	self.addTab(win2.containingTab);
-	
+	*/
 	//tab3 : about this app
 	var win3 = new AboutWindow(L('about'));
 	win3.containingTab = Ti.UI.createTab({
@@ -27,6 +31,9 @@ function ApplicationTabGroup(ReportWindow, SearchWindow,AboutWindow) {
 		//icon: '/images/ic_action_search.png',
 		window: win3
 	});
+	if (Titanium.Platform.name == 'iPhone OS'){
+		win3.containingTab.icon= '/images/KS_nav_ui.png';
+	}
 	self.addTab(win3.containingTab);
 	return self;
 };
--- Resources/ui/handheld/AboutWindow.js
+++ Resources/ui/handheld/AboutWindow.js
@@ -17,12 +17,10 @@ function AboutWindow(title) {
 			top:marginTop,
 			font: {fontSize:size}
 		});
-		Ti.API.info('data to send',text);
 	};
 	
 	self.createLine = function (view,texttitle,textdesc){
-		//var view = Ti.UI.createView({layout:'horizontal',top:10,left:0,right:10,height:'50'});
-		view.add(self.createLabel(texttitle,'white','12dp',10));
+		view.add(self.createLabel(texttitle,'DDDDDD','12dp',10));
 		view.add(self.createLabel(textdesc,'white','14dp',0));
 		return view;
 	}
@@ -30,12 +28,7 @@ function AboutWindow(title) {
 	var scrollable = Ti.UI.createScrollView({layout:'vertical'});
 	self.add(scrollable);
 	self.scrollable = scrollable;
-/*
-author,author_name
-special_thanks,special_thanks_to
-license,license_type
-sourcecode,sourcecode_url
-*/
+
 	self.createLine(scrollable,L('author'),L('author_name'));
 	self.createLine(scrollable,L('special_thanks'),L('special_thanks_to'));
 	self.createLine(scrollable,L('license'),L('license_type'));
